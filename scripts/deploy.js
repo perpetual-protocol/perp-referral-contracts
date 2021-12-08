8d0d12477c5f643bd375f9925ca97f39dd4daea7
1saf <me@ansaf.net>
2021-12-08 17:45:32 +1100

    deploy commands

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,12 +6,6 @@ async function main() {
   const referralV2 = await upgrades.deployProxy(PerpReferralV2, [], { initializer: 'init' });
   await referralV2.deployed();
   console.log("ReferrerV2 deployed to:", referralV2.address);
-
-  // const PerpetualProtocolReferrer = await ethers.getContractFactory("PerpetualProtocolReferrer");
-  // console.log("Deploying PerpeturalProtocolReferrer...");
-  // const referrer = await PerpetualProtocolReferrer.deploy();
-  // await referrer.deployed();
-  // console.log("Referrer deployed to:", referrer.address);
 }
 
 main()
--- scripts/editcontract.js
+++ scripts/editcontract.js
@@ -7,12 +7,8 @@ async function main() {
     const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
     const contract = new ethers.Contract('0xcf76A8365A218D799f36030d89f86C8FBCC65a6E', abi, wallet);
 
-    // const tx = await contract['createReferralCode']('0xb00F278820859B7865CEE00AEB19B8a2AD97C7B1', 'perpofficial')
     const tx = await contract['_importPartner']('0x59737202A321Aa374D54a487997Fad625C2c9DC9', 'TEST1234', [])
     await tx.wait(2);
-
-    // const res = await contract['uncappedPartners']('0x512472840327530eA03cCE6F58966B221f3a8b6a');
-    // console.log('res', res);
 }
 
 main()
